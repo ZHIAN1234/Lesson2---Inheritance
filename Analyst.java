46638eeba51841de539fd98cf14a1b9c8e81e1c9
ZHIAN1234 <zhianfrancisco257@gmail.com>
2025-04-11 16:28:54 +0800

    Delete Analyst.java

--- Analyst.java
+++
@@ -1,9 +0,0 @@
-public class Analyst extends Employee {
-
-    public Analyst(String name, double salary, int age) {
-        super(name, age, salary);
-    }
-    public double getAnnualBonus() {
-        return super.salary * .05;
-    }
-}
